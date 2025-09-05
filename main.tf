615228784ad96cdc39c8e87c560938e81d28686b
Jitender Kumar <thejitenderk@gmail.com>
2025-09-05 22:21:13 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.33.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "6427aa49-28ab-4afb-8e17-6f227b30229c"
+}
+
+resource "azurerm_resource_group" "tf-test" {
+  name     = "tf-test"
+  location = "UK South"
+}
+
